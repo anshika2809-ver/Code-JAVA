eb86d44459e114d5b932f62a1d1f5fa6c1af1990
Anshika verma <68806412+anshika2809-ver@users.noreply.github.com>
2024-06-29 15:41:18 +0530

    Update and rename Q11 to Q11.java

--- DSA_sheet/Q11.java
+++ DSA_sheet/Q11.java
@@ -26,4 +26,8 @@ class Solution {
         return trappedwater;
         
     }
+    public static void main(String[] args) {
+    int height[]={0,1,0,2,1,0,1,3,2,1,2,1};
+    System.out.println(trap(height));
+  }
 }
